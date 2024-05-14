6cfac7f822f36b1138322b599bf862eac9c4d51a
JavohirMuradov <javohirmuradov030@gmail.com>
2024-05-15 02:11:25 +0500

    header readygit add . ����

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,11 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next';
 import { FaFacebookSquare, FaTelegram, FaYoutube, FaInstagram } from "react-icons/fa";
-import { IconButton } from "@material-tailwind/react";
+import {
+  IconButton, Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
 import logo from '../../../public/logo.png';
 import ru from '../../../public/ru.png';
 import uk from '../../../public/uk.png';
 import { NavLink, useLocation } from 'react-router-dom';
+
+function Icon({ id, open }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      className={`h-7 w-7 inline-block align-text-bottom text-yellow ${id === open ? "rotate-180" : ""} transition-transform duration-300`}
+    >
+      <path
+        fillRule="evenodd"
+        d="M6.293 8.293a1 1 0 011.414 0L10 10.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
 
 const Header = () => {
   var { t } = useTranslation()
@@ -19,23 +40,8 @@ const Header = () => {
     );
   }, []);
   const [li, setLi] = useState(false);
-  const [li1, setLi1] = useState(false);
-  const [li2, setLi2] = useState(false);
-  const [li3, setLi3] = useState(false);
-  const [li4, setLi4] = useState(false);
-  var funcClassName1 = (li, path) => {
-    var className = `relative before:absolute before:w-1/3 before:scale-x-${li === true || pathname === path ? "100" : null} before:left-0 before:bg-[#f7c35f] before:-bottom-[2px] after:absolute after:w-1/3 after:bg-[#f7c35f] after:right-0 after:-bottom-[2px] after:scale-x-${li === true || pathname === path ? "100" : null} duration-700`
-    return className
-
-  }
-  var funcClassName = (path, className, dependencie) => {
-    var classs = pathname === path || dependencie == true ? `h-${className}` : null;
-    return classs
-  }
-  var div = (w1, w2, path, dependencie) => {
-    var div = pathname === path || dependencie == true ? <div className={`absolute right-1/3 left-1/3 bottom-[5px] w-1/3 after:absolute after:w-${w1} after:bg-[#f7c35f] after:-rotate-[20deg] after:-bottom-[4px] after:-left-[1px] after:${funcClassName("/", "[2px]", li)} before:absolute before:w-${w2} before:bg-[#f7c35f] before:rotate-[20deg] before:-right-[1px] before:-bottom-[4px] before:${funcClassName("/", "[2px]", li1)} duration-300`}></div> : ""
-    return div
-  }
+  const [open, setOpen] = useState(0);
+  const handleOpen = (value) => setOpen(open === value ? 0 : value);
 
   return (
     <header>
@@ -84,32 +90,62 @@ const Header = () => {
         </div>
         <div className='hidden xl:block'>
           <ul className='flex flex-row gap-5 items-center justify-center h-full'>
-            <li onMouseEnter={() => setLi(true)} onMouseLeave={() => setLi(false)} className={`text-gray hover:text-black duration-300 ${funcClassName1(li, "/")} before:${funcClassName("/", "[2px]", li)} after:${funcClassName("/", "[2px]", li)}`}>
+            <li className={`group hover:text-black text-gray duration-300 relative`}>
+              <div className={`${pathname === "/" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 w-1/3 transition duration-700 bg-yellow absolute h-[2px] left-0 -bottom-[2px]`}></div>
+              <div className={`${pathname === "/" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 absolute h-[2px] w-1/3 transition duration-700 bg-yellow right-0 -bottom-[2px]`}></div>
               <NavLink to={"/"}>{t("header.bottomHeader.home")}</NavLink>
-              {div(3, 3, "/", li)}
+              <div className={`${pathname === "/" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 w-1/6 transition duration-700 bg-yellow absolute h-[2px] bottom-0 -rotate-[20deg] left-1/3`}></div>
+              <div className={`${pathname === "/" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 absolute h-[2px] w-1/6 transition duration-700 bg-yellow bottom-0 right-1/3 rotate-[20deg] `}></div>
             </li>
-            <li onMouseEnter={() => setLi1(true)} onMouseLeave={() => setLi1(false)} className={`text-gray hover:text-black duration-300 ${funcClassName1(li1, "/certificate")} before:${funcClassName("/certificate", "[2px]", li1)} after:${funcClassName("/certificate", "[2px]", li1)}`}>
-              <button>{t("header.bottomHeader.about")}</button>
-              {div(4, 4, "/certificate", li1)}
-              <button>
-                <NavLink to={"/certificate"} className={`absolute -bottom-10 rounded-md -right-4 -left-4 shadow-md py-2 px-4 flex flex-row items-center justify-center scale-y-0 duration-300 ${li1 == true ? "scale-y-100" : null}`}>{t("header.bottomHeader.certificate")}</NavLink>
+            <li onMouseEnter={() => setLi(true)} onMouseLeave={() => setLi(false)} className={`group hover:text-black text-gray relative`}>
+              <button className='flex flex-row items-center'>{t("header.bottomHeader.about")}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-17 w-7 inline-block align-text-bottom text-yellow"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M6.293 8.293a1 1 0 011.414 0L10 10.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
               </button>
+              <div className={`${pathname === "/certificate" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 w-1/3 transition duration-700 bg-yellow absolute h-[2px] left-0 -bottom-[2px]`}></div>
+              <div className={`${pathname === "/certificate" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 absolute h-[2px] w-1/3 transition duration-700 bg-yellow right-0 -bottom-[2px]`}></div>
+              <div className={`${pathname === "/certificate" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 w-1/6 transition duration-700 bg-yellow absolute h-[2px] bottom-[1px] -rotate-[20deg] left-1/3`}></div>
+              <div className={`${pathname === "/certificate" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 absolute h-[2px] w-1/6 transition duration-700 bg-yellow bottom-[1px] right-1/3 rotate-[20deg] `}></div>
+              <NavLink to={"/certificate"} className={`${li === true ? "scale-y-100" : "scale-0"} absolute -bottom-10 rounded-md -right-4 -left-4 shadow-md py-2 px-4 flex flex-row items-center justify-center duration-300`}>
+                <button>
+                  {t("header.bottomHeader.certificate")}
+                </button>
+              </NavLink>
             </li>
-            <li onMouseEnter={() => setLi2(true)} onMouseLeave={() => setLi2(false)} className={`text-gray hover:text-black duration-300 ${funcClassName1(li2, "/products")} before:${funcClassName("/products", "[2px]", li2)} after:${funcClassName("/products", "[2px]", li2)}`}>
+            <li className={`group hover:text-black text-gray relative`}>
+              <div className={`${pathname === "/products" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 w-1/3 transition duration-700 bg-yellow absolute h-[2px] left-0 -bottom-[2px]`}></div>
+              <div className={`${pathname === "/products" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 absolute h-[2px] w-1/3 transition duration-700 bg-yellow right-0 -bottom-[2px]`}></div>
+              <div className={`${pathname === "/products" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 w-1/6 transition duration-700 bg-yellow absolute h-[2px] bottom-0 -rotate-[20deg] left-1/3`}></div>
+              <div className={`${pathname === "/products" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 absolute h-[2px] w-1/6 transition duration-700 bg-yellow bottom-0 right-1/3 rotate-[20deg] `}></div>
               <NavLink to={"/products"}>{t("header.bottomHeader.products")}</NavLink>
-              {div(4, 3, "/products", li2)}
             </li>
-            <li onMouseEnter={() => setLi3(true)} onMouseLeave={() => setLi3(false)} className={`text-gray hover:text-black duration-300 ${funcClassName1(li3, "/news")} before:${funcClassName("/news", "[2px]", li3)} after:${funcClassName("/news", "[2px]", li3)}`}>
+            <li className={`group hover:text-black text-gray relative`}>
+              <div className={`${pathname === "/news" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 w-1/3 transition duration-700 bg-yellow absolute h-[2px] left-0 -bottom-[2px]`}></div>
+              <div className={`${pathname === "/news" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 absolute h-[2px] w-1/3 transition duration-700 bg-yellow right-0 -bottom-[2px]`}></div>
+              <div className={`${pathname === "/news" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 w-1/6 transition duration-700 bg-yellow absolute h-[2px] bottom-0 -rotate-[20deg] left-1/3`}></div>
+              <div className={`${pathname === "/news" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 absolute h-[2px] w-1/6 transition duration-700 bg-yellow bottom-0 right-1/3 rotate-[20deg] `}></div>
               <NavLink to={"/news"}>{t("header.bottomHeader.news")}</NavLink>
-              {div(3, 3, "/news", li3)}
             </li>
-            <li onMouseEnter={() => setLi4(true)} onMouseLeave={() => setLi4(false)} className={`text-gray hover:text-black duration-300 ${funcClassName1(li4, "/contact")} before:${funcClassName("/contact", "[2px]", li4)} after:${funcClassName("/contact", "[2px]", li4)}`}>
+            <li className={`group hover:text-black text-gray relative`}>
+              <div className={`${pathname === "/contact" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 w-1/3 transition duration-700 bg-yellow absolute h-[2px] left-0 -bottom-[2px]`}></div>
+              <div className={`${pathname === "/contact" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 absolute h-[2px] w-1/3 transition duration-700 bg-yellow right-0 -bottom-[2px]`}></div>
+              <div className={`${pathname === "/contact" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 w-1/6 transition duration-700 bg-yellow absolute h-[2px] bottom-0 -rotate-[20deg] left-1/3`}></div>
+              <div className={`${pathname === "/contact" ? "scale-y-100" : "scale-y-0"} group-hover:scale-y-100 absolute h-[2px] w-1/6 transition duration-700 bg-yellow bottom-0 right-1/3 rotate-[20deg] `}></div>
               <NavLink to={"/contact"}>{t("header.bottomHeader.contact")}</NavLink>
-              {div(4, 4, "/contact", li4)}
             </li>
           </ul>
         </div>
-        <div className='flex flex-row items-center gap-5'>
+        <div className='md:flex flex-row items-center gap-5 hidden'>
           <img src={ru} alt="" />
           <img src={uk} alt="" />
           <button className='flex flex-row text-white py-5 px-4 bg-[#f7c35f] rounded-xl gap-1'>
@@ -130,9 +166,27 @@ const Header = () => {
           variant="text"
           className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden duration-300"
           ripple={false}
-          onClick={() => setOpenNav(!openNav)}
+          onClick={() => setOpenNav(true)}
         >
-          {openNav ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </IconButton>
+      </nav >
+      <div className={`absolute top-0 bg-white py-10 w-1/3 z-20 h-full flex flex-col items-center ${openNav ? "translate-x-0" : "-translate-x-full"} duration-300`}>
+        <div className='flex flex-row justify-between items-center gap-5  w-10/12'>
+          <img src={logo} alt="logo" className='w-1/2' />
+          <button onClick={() => setOpenNav(false)}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -147,24 +201,65 @@ const Header = () => {
                 d="M6 18L18 6M6 6l12 12"
               />
             </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          )}
-        </IconButton>
-      </nav>
-    </header >
+          </button>
+        </div>
+        <ul className='flex flex-col gap-5 items-start justify-center w-10/12 pt-10'>
+          <li className={`group text-green duration-300 relative`}>
+            <NavLink to={"/"}>{t("header.bottomHeader.home")}</NavLink>
+          </li>
+          <Accordion open={open === 1} icon={<Icon id={1} open={open} />}>
+            <AccordionHeader className='border-b-0 text-green text-base font-normal py-0 px-0 justify-start hover:text-green' onClick={() => handleOpen(1)}>{t("header.bottomHeader.about")}</AccordionHeader>
+            <AccordionBody className="py-0 pt-4">
+              <NavLink to={"/certificate"} className="text-green text-base font-normal mb-0">
+                <button>
+                  {t("header.bottomHeader.certificate")}
+                </button>
+              </NavLink>
+            </AccordionBody>
+          </Accordion>
+          <li className={`group text-green relative`}>
+            <NavLink to={"/products"}>{t("header.bottomHeader.products")}</NavLink>
+          </li>
+          <li className={`group text-green relative`}>
+            <NavLink to={"/news"}>{t("header.bottomHeader.news")}</NavLink>
+          </li>
+          <li className={`group text-green relative`}>
+            <NavLink to={"/contact"}>{t("header.bottomHeader.contact")}</NavLink>
+          </li>
+        </ul>
+        <div className='flex flex-row items-center justify-start w-10/12 gap-3 mt-5'>
+          <svg width="172" height="120" viewBox="0 0 172 120" className="w-7 h-8 text-yellow" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M83.1769 77.0836L22.8753 28.5043C19.5687 25.8405 21.4523 20.5 25.6983 20.5L146.302 20.5C150.548 20.5 152.431 25.8405 149.125 28.5043L88.8231 77.0836C87.1751 78.4112 84.8249 78.4112 83.1769 77.0836Z" fill="#f7c35f" stroke="#f7c35f" />
+            <path fillRule="evenodd" clipRule="evenodd" d="M22 20C15.9249 20 11 24.9249 11 31V109C11 111.44 11.7942 113.694 13.1382 115.518L84.1046 57.6746L86 56.1297L87.8954 57.6746L158.862 115.518C160.206 113.694 161 111.44 161 109V31C161 24.9249 156.075 20 150 20H22ZM156.762 117.677L155.932 117L86 60L16.0684 117L15.2381 117.677C17.1035 119.133 19.4504 120 22 120H150C152.55 120 154.897 119.133 156.762 117.677Z" fill="#f7c35f" />
+            <path d="M85.0523 81.1627L86 81.9352L86.9477 81.1627L160.56 21.1627L163.827 18.5H159.612L12.3878 18.5H8.17337L11.4402 21.1627L85.0523 81.1627Z" stroke="#F6F5F2" strokeWidth="3" />
+          </svg>
+          <svg width="100" height="100" className="w-5 h-5" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M100 50C100 77.6142 77.6142 100 50 100C22.3858 100 0 77.6142 0 50C0 22.3858 22.3858 0 50 0C77.6142 0 100 22.3858 100 50ZM52 47.0378V10H48V49.8146V50H48.321L82.1404 69.5256L84.3428 65.711L52 47.0378Z" fill="#f7c35f" />
+          </svg>
+        </div>
+        <div className='flex flex-row items-center justify-between w-10/12 mt-10'>
+          <div className='flex flex-row items-center gap-2'>
+            <img src={ru} alt="" />
+            <img src={uk} alt="" />
+          </div>
+          <div className="flex flex-row items-center gap-2">
+            <a href="https://mehrigiyo.org/en/">
+              <FaFacebookSquare className="text-gray hover:text-[#f7c35f] duration-500 cursor-pointer h-5 w-5" />
+            </a>
+            <a href="https://mehrigiyo.org/en/">
+              <FaTelegram className="text-gray hover:text-[#f7c35f] duration-500 cursor-pointer h-5 w-5" />
+            </a>
+            <a href="https://mehrigiyo.org/en/">
+              <FaYoutube className="text-gray hover:text-[#f7c35f] duration-500 cursor-pointer h-5 w-5" />
+            </a>
+            <a href="https://mehrigiyo.org/en/">
+              <FaInstagram className="text-gray hover:text-[#f7c35f] duration-500 cursor-pointer h-5 w-5" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className={`${openNav ? "translate-x-0" : "-translate-x-full"} delay-100 duration-300 h-full w-full bg-green bg-opacity-50 absolute top-0 left-0 z-10`}></div>
+    </header>
   )
 }
 
